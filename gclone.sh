56fa23cb07fe10db70b0666f0bde10f13c89e0e8
oneindex <47175944+oneindex@users.noreply.github.com>
2020-02-08 14:47:19 +0800

    Create gclone.sh

---
+++ gclone.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if ! command -v systemctl >/dev/null 2>&1; then
+    echo "> Sorry but this scripts is only for Linux with systemd, eg: Ubuntu 16.04+/Centos 7+ ..."
+    exit 1
+fi
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root" 
+    exit 1
+fi
+
+HOSTIP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+CLDBIN=/usr/bin/gclone
+OSARCH=$(uname -m)
+case $OSARCH in 
+    x86_64)
+        BINTAG=Linux_x86_64
+        ;;
+    i*86)
+        BINTAG=Linux_i386
+        ;;
+    arm64)
+        BINTAG=Linux_arm64
+        ;;
+    arm*)
+        BINTAG=Linux_armv6
+        ;;
+    *)
+        echo "unsupported OSARCH: $OSARCH"
+        exit 1
+        ;;
+esac
+
+wget -qO- https://api.github.com/repos/oneindex/gclone/releases/latest \
+| grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
+| wget --no-verbose -i- -O- | gzip -d -c > ${CLDBIN}
+chmod 0755 ${CLDBIN}
+
+gclone version

b248be9098b47baaba0f506b84f9449440fdd8cb
oneindex <47175944+oneindex@users.noreply.github.com>
2020-02-13 16:21:48 +0800

    Update gclone.sh

--- gclone.sh
+++ gclone.sh
@@ -36,7 +36,7 @@ case $OSARCH in
         ;;
 esac
 
-wget -qO- https://api.github.com/repos/oneindex/gclone/releases/latest \
+wget -qO- https://api.github.com/repos/donwa/gclone/releases/latest \
 | grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
 | wget --no-verbose -i- -O- | gzip -d -c > ${CLDBIN}
 chmod 0755 ${CLDBIN}
